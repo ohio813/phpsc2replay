cfa51414265b6fbd77fc49d92963ba28930dd4a9
lauri.virkamaki <lauri.virkamaki@localhost>
2010-10-24 21:59:47 +0300

    Seperated map hashes and map locale data in sc2replayutils.php to avoid duplicate data. Added improved English map name parsing, now uses both map hashes as well as searches the localized name in case the hash is not found Added experimental recorder detection, this is not fully tested yet. Recorder player array available via getRecorder() member function Added improved winner detection. The algorithm should now be as accurate as possible.

--- php/upload_file.php
+++ php/upload_file.php
@@ -196,10 +196,13 @@ if (isset($_FILES['userfile'])) {
 				$b = $a->parseReplay();
 				$parseDurationString .= sprintf("Parsed replay in %d ms.<br />\n",((microtime_float() - $start)*1000));
 				$players = $b->getPlayers();
+				$recorder = $b->getRecorder();
 				echo sprintf("Map name: %s, Game length: %s<br />\n",$b->getMapName(),$b->getFormattedGameLength());
 				echo sprintf("Team size: %s, Game speed: %s<br />\n",$b->getTeamSize(), $b->getGameSpeedText());
 				echo sprintf("Realm: %s<br />\n",$b->getRealm());
-				echo sprintf("Date and time played: %s",date('jS \of F Y \a\t H:i' ,$b->getCtime()));
+				echo sprintf("Date and time played: %s<br />\n",date('jS \of F Y \a\t H:i' ,$b->getCtime()));
+				if ($recorder != null)
+					echo sprintf("Replay recorded by: %s (EXPERIMENTAL!)<br />\n",$recorder['name']);
 				$apmString = "<b>APM graphs</b><br />\n";
 				$obsString = "";
 				$obsCount = 0;
