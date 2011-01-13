1af694a7f08435f9492416b1ce649334bd2e8d39
Zsolt Dollenstein <zsol@starcraft2.hu>
2011-01-13 08:37:13 +0000

    Implement infrastructure for parsing multiple versions of replay files. The base class is SC2Replay as defined in php/sc2replay.php. The implementation of different versions are found in the "v<whatever>" directories as a single sc2replay.php file that contains SC2ReplayImpl which extends SC2Replay and overrides the necessary functions. The "vdefault" directory contains an implementation which uses the default methods from the base class, this is to be used with all pre-1.2.0-17326 builds. The logic which decides what version to use is implemented as a static function makeReplay in the SC2Replay class. This returns the correct SC2ReplayImpl object, that has the same interface as SC2Replay and should be used in the same way.

---
+++ php/vdefault/sc2replay.php
@@ -0,0 +1,37 @@
+<?php
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+class SC2ReplayImpl extends SC2Replay {
+	
+	function __construct() {
+		$this->players = array();
+		$this->gameLength = 0;
+		$this->mapName = NULL;
+		$this->gameSpeed = 0;
+		$this->teamSize = NULL;
+		$this->realTeamSize = NULL;
+		$this->debug = false;
+		$this->debugNewline = "<br />\n";
+		$this->winnerKnown = false;
+		$this->unitsDict = array();
+		$this->mapHash = null;
+		$this->recorderId = 0;
+	}
+
+	// All other functions use the default implementation from base class
+}
+
+
+?>
